805fa06fa89766dea31f53e7510699c3894e6336
Oliver <omh7431@rit.edi>
2021-02-14 18:34:30 -0500

    fixed streaming

--- src/server.js
+++ src/server.js
@@ -14,6 +14,9 @@ const onRequest = (request, response) => {
     case '/party.mp4':
       mediaHandler.getParty(request, response);
       break;
+    case '/bird.mp4':
+      mediaHandler.getBird(request, response);
+      break;
     default:
       htmlHandler.getIndex(request, response);
       break;
